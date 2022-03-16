5cac79c5e61111675a65bc00cab212fee102380a
AidaGube <aidagube@gmail.com>
2022-03-16 20:34:46 +0300

    Sdelala component Stock ne do konca

--- src/components/Stock/StockItem.vue
+++ src/components/Stock/StockItem.vue
@@ -1,44 +1,44 @@
 <template>
-  <div class="container">
-          <div class="title"><h1>АКЦИИ</h1></div>
+<div class="stock__list">
+  <div class="stock__items">
+    <div class="col-4">
+      <div class="stock__item">
+        <div class="stock__image">
+          <img :src="StockImgMeat" alt="item-image" />
         </div>
-        <hr />
-        <section class="stock__list">
-         <div class="container">
-            <div class="stock__items">
-            <div class="col-4">
-              <div class="stock__item">
-                <div class="stock__image">
-                  <img :src="StockImgMeat" alt="item-image" />
-                </div>
-                <div class="stock__description">
-                  <div class="stock__header">
-                    <div class="stock__title">Без мяса? Здесь!</div>
-                  </div>
-                  <div class="stock__subtitle">
-                    Самое время попробовать «Сырную» пиццу, «Маргариту», пиццу
-                    «Овощи и грибы», Пасту Четыре сыра, Томатный суп с гренками,
-                    Грибной Стартер, Рулетики с сыром, Картофель из печи,
-                    Картофельные оладьи или Греческий салат. Выберите свой вкус!
-                  </div>
-                  <div class="stock__footer">
-                    <div class="stock__data">до 31 июля.</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            </div>
-         </div>
-        </section>
+        <div class="stock__description">
+          <div class="stock__header">
+            <div class="stock__title">{{ label }}</div>
+          </div>
+          <div class="stock__subtitle">
+            Самое время попробовать «Сырную» пиццу, «Маргариту», пиццу «Овощи и
+            грибы», Пасту Четыре сыра, Томатный суп с гренками, Грибной Стартер,
+            Рулетики с сыром, Картофель из печи, Картофельные оладьи или
+            Греческий салат. Выберите свой вкус!
+          </div>
+          <div class="stock__footer">
+            <div class="stock__data">до 31 июля.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
 </template>
 
 <script>
 import StockImgMeat from "@/assets/images/stock-meat.svg";
+// import StockImgKombo from "@/assets/images/stock-meat.svg";
+// import StockImgSyrniki from "@/assets/images/stock-meat.svg";
 export default {
-  props: ["items", "label"],
-  setup(props) {
+  props: ["label"],
+  setup() {
     return {
-      StockImgMeat,
+      StockImgMeat
+      // imgUrl:[
+      //   {id: 1,
+      //   image: StockImgMeat,}
+      // ]
     };
   },
 };
--- src/components/Stock/StockList.vue
+++ src/components/Stock/StockList.vue
@@ -1,12 +1,57 @@
 <template>
-  <StockItem/>
+  <div class="container">
+    <Title title="АКЦИИ" />
+    <hr />
+  </div>
+  <div class="wrapper">
+    <div class="stock__list">
+    <div class="stock__items">
+      <div class="col-4">
+        <StockItem :label="firstName" />
+      </div>
+      <div class="col-4">
+        <StockItem :label="secondName" />
+      </div>
+      <div class="col-4">
+        <StockItem :label="threeName" />
+      </div>
+      <div class="col-4">
+         <StockItem :label="secondName" />
+      </div>
+      <div class="col-4">
+        <StockItem :label="threeName" />
+      </div>
+      <div class="col-4">
+         <StockItem :label="firstName" />
+      </div>
+      <div class="col-4">
+        <StockItem :label="threeName" />
+      </div>
+      <div class="col-4">
+         <StockItem :label="firstName" />
+      </div>
+      <div class="col-4">
+        <StockItem :label="secondName" />
+      </div>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
-import StockItem from './StockItem.vue';
+import Title from "@/components/Title/Title";
+import StockItem from "./StockItem.vue";
+
 export default {
-components: {StockItem}
-}
+  components: { Title, StockItem },
+  setup() {
+    return {
+      firstName: "Без мяса? Здесь!",
+      secondName: "Выгодное комбо c напитками",
+      threeName:"Сырный бортик",
+    }
+  }
+};
 </script>
 
 <style>
