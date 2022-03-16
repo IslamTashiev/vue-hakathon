f23b2da7f4b028be2028f3d11288f4fc4ceee575
IslamTashiev <ueaten04@gmail.com>
2022-03-16 21:20:45 +0600

    create db.json and more components

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,12 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
+body {
+  background: linear-gradient(360deg, #211f20 0%, #44403f 100%) no-repeat;
+
+  &._blocked {
+    overflow: hidden;
+  }
+}
 </style>
---
+++ src/components/ProductList/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <Title title="ХОЛОДНЫЕ ЗАКУСКИ" /> 
+    </div>
+    <div class="product__list">
+        <ProductItem />
+    </div>
+</template>
+
+<script>
+import Title from "@/components/Title/Title"
+import ProductItem from "./ProductItem"
+import { ref } from '@vue/reactivity'
+export default {
+    components: {Title,ProductItem},
+    setup() {
+        const products = ref([])
+
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
