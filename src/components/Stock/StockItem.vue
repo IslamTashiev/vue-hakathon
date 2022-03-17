e173e9ad86341e3c2024de989e4202df489b26e8
AidaGube <aidagube@gmail.com>
2022-03-17 14:14:15 +0300

    adaptiv Stock

--- src/components/Stock/StockItem.vue
+++ src/components/Stock/StockItem.vue
@@ -1,45 +1,37 @@
 <template>
-<div class="stock__list">
-  <div class="stock__items">
-    <div class="col-4">
-      <div class="stock__item">
-        <div class="stock__image">
-          <img :src="item.image"/>
-        </div>
-        <div class="stock__description">
-          <div class="stock__header">
-            <div class="stock__title">{{ item.tatelName }}</div>
+    <div class="stock__items col-4">
+        <div class="stock__item">
+          <div class="stock__image">
+            <img :src="item.image" />
           </div>
-          <div class="stock__subtitle">
-            Самое время попробовать «Сырную» пиццу, «Маргариту», пиццу «Овощи и
-            грибы», Пасту Четыре сыра, Томатный суп с гренками, Грибной Стартер,
-            Рулетики с сыром, Картофель из печи, Картофельные оладьи или
-            Греческий салат. Выберите свой вкус!
-          </div>
-          <div class="stock__footer">
-            <div class="stock__data">до 31 июля.</div>
+          <div class="stock__description">
+            <div class="stock__header">
+              <div class="stock__title">{{ item.tatelName }}</div>
+            </div>
+            <div class="stock__subtitle">
+              Самое время попробовать «Сырную» пиццу, «Маргариту», пиццу «Овощи
+              и грибы», Пасту Четыре сыра, Томатный суп с гренками, Грибной
+              Стартер, Рулетики с сыром, Картофель из печи, Картофельные оладьи
+              или Греческий салат. Выберите свой вкус!
+            </div>
+            <div class="stock__footer">
+              <div class="stock__data">до 31 июля.</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-    </div>
 </template>
 
 <script>
-// import StockImgMeat from "@/assets/images/stock-meat.svg";
-// // import StockImgKombo from "@/assets/images/stock-meat.svg";
-// // import StockImgSyrniki from "@/assets/images/stock-meat.svg";
 export default {
-   props: {
+  props: {
     item: {
       type: Object,
-      reqired: true
-    }
+      reqired: true,
+    },
   },
   setup() {
-    return {
-    };
+    return {};
   },
 };
 </script>
--- src/components/Stock/StockList.vue
+++ src/components/Stock/StockList.vue
@@ -3,64 +3,57 @@
     <Title title="АКЦИИ" />
     <hr />
   </div>
-  <div class="wrapper">
-    <div class="stock__list">
-    <div class="stock__items">
-      <div class="col-4">
-        <StockItem v-for="item in items" :key="item.id" :item="item"/>
-      </div>
-      <div class="col-4">
-        <StockItem v-for="item in items" :key="item.id" :item="item"/>
-      </div>
-      <div class="col-4">
-        <StockItem v-for="item in items" :key="item.id" :item="item"/>
+  <div class="stock__container">
+      <div class="stock__items">
+        <StockItem v-for="item in items" :key="item.id" :item="item" />
+        <StockItem v-for="item in items" :key="item.id" :item="item" />
+        <StockItem v-for="item in items" :key="item.id" :item="item" />
       </div>
     </div>
-  </div>
-  </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { ref } from "@vue/reactivity";
 import Title from "@/components/Title/Title";
 import StockItem from "./StockItem.vue";
 import StockImgMeat from "@/assets/images/stock-meat.svg";
 import StockImgKombo from "@/assets/images/stock-kombo.svg";
 import StockImgSyrniki from "@/assets/images/stock-syrniki.svg";
+
 export default {
   components: { Title, StockItem },
   setup() {
-     const items = ref([
+    const items = ref([
+      {
+        id: 2,
+        tatelName: "Выгодное комбо c напитками",
+        image: StockImgKombo,
+      },
       {
         id: 1,
         tatelName: "Без мяса? Здесь!",
         image: StockImgMeat,
       },
       {
-        id: 2,
-        tatelName: "Выгодное комбо c напитками",
-        image: StockImgKombo,
-      },
-       {
         id: 3,
-        tatelName:"Сырный бортик",
+        tatelName: "Сырный бортик",
         image: StockImgSyrniki,
       },
-    ])
+    ]);
 
     const handleSubmit = (items) => {
-      items.value.Math.sort(() => Math.random() - 0.5)
+      items.value.sort(() => 0.5 - Math.random());
+      console.log(items.value);
       // .sort(() => Math.random() - 0.5
-    }
+    };
 
     return {
-      items,handleSubmit
-    }
-  }
-}
-
+      items,
+      handleSubmit,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
