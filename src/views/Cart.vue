c7a86ff27e95dd55a4106316dbe449994e6c963d
AidaGube <aidagube@gmail.com>
2022-03-19 16:45:32 +0300

    dobavila component Buttons i izmenila contacts

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,13 +1,15 @@
 <template>
 <Header />
+<Button className="btn-gray" text="Оформить заказ" />
 <Footer />
 </template>
 
 <script>
 import Header from "@/components/Header/Header.vue";
 import Footer from "@/components/Footer/Footer.vue";
+import Button from "@/components/Buttons/Button.vue";
 export default {
-  components: {Header,Footer},
+  components: {Header,Footer,Button},
 };
 </script>
 
