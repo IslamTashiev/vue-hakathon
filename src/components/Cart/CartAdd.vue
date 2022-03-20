6c3d725cd68f31762106671001901043fa922487
AidaGube <aidagube@gmail.com>
2022-03-20 14:05:52 +0300

    cart

---
+++ src/components/Cart/CartAdd.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="cart__add-item">
+    <img
+      class="cart__add-image"
+      src="@/assets/images/add-img.svg"
+      alt="add-image"
+    />
+    <div class="cart__add-label">
+      <h4 class="cart__add-subtitle">КВАС АНАНАСОВЫЙ</h4>
+      <div class="cart__add-push">
+        Добавить
+        <img
+          class="cart__add-plus"
+          src="@/assets/images/add-plus.svg"
+          alt="title"
+        />
+      </div>
+      <span class="cart__add-price"> 1640 ₽</span>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
